f75d21c8f27e1621ebb3f05b144ced4df1b14d82
Valentin Hauner <valentin.hauner@maibornwolff.de>
2020-05-25 22:50:12 +0200

    Removes unnecessary error channel

--- src/main/rabbitmq/main.go
+++ src/main/rabbitmq/main.go
@@ -12,10 +12,6 @@ func Connect(amqpURI string, enablePublishingConfirms bool) (*amqp.Connection, *
 		return nil, nil, nil, fmt.Errorf("Failed to dial: %s", err)
 	}
 
-	go func() {
-		fmt.Printf("Closing: %s", <-conn.NotifyClose(make(chan *amqp.Error)))
-	}()
-
 	channel, err := conn.Channel()
 	if err != nil {
 		return nil, nil, nil, fmt.Errorf("Failed to open channel: %s", err)
